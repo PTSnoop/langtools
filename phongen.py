b70fcff0b792692ed3ff919fd073a80b6b0a4d1c
Andrew Boreham <ptsnoop@gmail.com>
2013-05-10 10:01:19 +0100

    Phongen now distributes things more realistically

--- phongen.py
+++ phongen.py
@@ -7,36 +7,64 @@ import sys
 C = [
 	'pp',	'tt',	'kk',	
 	'ph',	'th',	'kh',	
-	#'b',	'd',	'g',	
-	'f'	's',	'c',
+	'f',	's','z','c',
 	'm',	'n',	'ng',
-		'i','l','r',
+	'u',	'i','l','r',
 ]
+C1 = [ 's','th','ph','n','c','n','l','kh','tt','pp','ng','m','r','f','i','u','kk','z']
+C2 = [ 'n','s','c','th','l','ph','kh','z','ng','m','f','kk','pp','tt','r']
+
+I = ['i','l','n','s']
+
 V = [
 	'i','ʉ',      'u',
 	 'e','ø',  'o',
 	     'a',
 ]
-D = ['au','ai','ia','ua']
+V1 = ['i','a','e','o','ia','ø','ai','u','ua','au','ʉ']
+iV = ['a','e','o','ø','u','ʉ','au','ai']
 
-word = ""
-if len(sys.argv) > 1:
-	noOfWords = int(sys.argv[1])
-else:
-	noOfWords = 1
-while True:
-	word += random.choice(C)
+def DistPick(A):
+	size = len(A)+1
+	B = []
+	for thing in range(len(A)):
+		B.append( 100-(1-((1/size)*thing) ))
+
+	picknum = random.randint(0,sum(B))
+
+	for thing in range(len(B)):
+		if picknum < B[thing]:
+			return A[thing]
+		else:
+			picknum -= B[thing]
+	return A[0]
+
+def NewWord():
+	word = ""
+	word += DistPick(C1)
 	if random.random() < 0.3:
-		word += "i"
-	if random.random() < 0.7:
-		word += random.choice(V)
+		word += DistPick(I)
+		word += DistPick(iV)
 	else:
-		word += random.choice(D)
-	if random.random() < 0.7:
-		if random.random() < 0.5:
-			word += random.choice(C)
+		word += DistPick(V1)
+	if random.random() < 0.4:
+		word += DistPick(C2)
+	if random.random() < 0.3:
+		word += NewWord()
+	return word
+
+if __name__ == "__main__":
+
+	word = ""
+	if len(sys.argv) > 1:
+		noOfWords = int(sys.argv[1])
+	else:
+		noOfWords = 1
+
+	while True:
+		word += NewWord()
 		word += " "
 		noOfWords -= 1
 		if noOfWords <= 0:
 			break
-print word
+	print word
